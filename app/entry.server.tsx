bf48e984bf79285efc37c323ca9cc477280fc46d
DavinciAPI <cherifahcene123@gmail.com>
2025-01-06 06:39:22 +0300

    Update entry.server.tsx

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,32 +1,41 @@
-import { renderToReadableStream } from 'react-dom/server.browser';
+import type { AppLoadContext, EntryContext } from '@remix-run/cloudflare';
 import { RemixServer } from '@remix-run/react';
-import type { EntryContext } from '@remix-run/cloudflare';
+import { isbot } from 'isbot';
+import { renderToReadableStream } from 'react-dom/server.browser';
+import { renderHeadToString } from 'remix-island';
+import { Head } from './root';
 
 export default async function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
-  remixContext: EntryContext
+  remixContext: EntryContext,
+  _loadContext: AppLoadContext,
 ) {
+  const callbackName = isbot(request.headers.get('user-agent'))
+    ? 'onAllReady'
+    : 'onShellReady';
+
+  const head = await renderHeadToString({ request, remixContext, Head });
+  
+  let didError = false;
+
   const stream = await renderToReadableStream(
     <RemixServer context={remixContext} url={request.url} />,
     {
-      onShellReady() {
+      [callbackName]() {
         responseHeaders.set('Content-Type', 'text/html');
         return new Response(stream, {
-          headers: responseHeaders,
-          status: responseStatusCode,
+          status: didError ? 500 : responseStatusCode,
+          headers: responseHeaders
         });
       },
       onError(error) {
+        didError = true;
         console.error(error);
-        responseHeaders.set('Content-Type', 'text/html');
-        return new Response('Internal Server Error', {
-          headers: responseHeaders,
-          status: 500,
-        });
-      },
+      }
     }
   );
+
   return stream;
 }
